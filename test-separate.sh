7af0ddb5c8c1582e2fdb616060c3adab674879b2
Long-Giang Vu <giangvl.cs@gmail.com>
2025-04-16 08:20:31 -0700

    feat: update

--- test-separate.sh
+++ test-separate.sh
@@ -17,15 +17,16 @@ export CPU_OFFLOAD="true"
 export OUTPUT_PATH="/mnt/d/consistent-3d-generation/samples/synthesized_images"
 export LIGHT_IMG_PATH="/mnt/d/consistent-3d-generation/samples/synthesized_images/pose.png"
 export LIGHT_PROMPT="A beautiful sunset over the ocean"
-export LIGHT_NUM_SAMPLES=5
+export LIGHT_NUM_SAMPLES=1
 export LIGHT_SEED=67890
-export LIGHT_STEPS=50
+export LIGHT_STEPS=25
 export LIGHT_A_PROMPT="best quality"
 export LIGHT_N_PROMPT="lowres, bad anatomy, bad hands, cropped, worst quality"
-export LIGHT_CFG=7.5
+export LIGHT_CFG=2.0
 export LIGHT_HIGHRES_SCALE=2.0
 export LIGHT_HIGHRES_DENOISE=0.75
 export LIGHT_BG_SOURCE="None"
+export LIGHT_LOWRES_DENOISE=0.9
 
 python synthesize_pose.py --input_img $INPUT_IMAGE --pose_prompt "$POSE_PROMPT" --pose_seed $POSE_SEED \
 --pose_guidance_scale $POSE_GUIDANCE_SCALE --pose_steps $POSE_STEPS --pose_infusenet_cond_scale $POSE_INFUSENET_COND_SCALE \
@@ -38,4 +39,4 @@ python synthesize_pose.py --input_img $INPUT_IMAGE --pose_prompt "$POSE_PROMPT" 
 python ic_light.py --input_img $LIGHT_IMG_PATH --out_path $OUTPUT_PATH --light_prompt "$LIGHT_PROMPT" --light_num_samples $LIGHT_NUM_SAMPLES --light_seed $LIGHT_SEED \
 --light_steps $LIGHT_STEPS --light_a_prompt "$LIGHT_A_PROMPT" --light_n_prompt "$LIGHT_N_PROMPT" \
 --light_cfg $LIGHT_CFG --light_highres_scale $LIGHT_HIGHRES_SCALE --light_highres_denoise $LIGHT_HIGHRES_DENOISE \
---light_bg_source $LIGHT_BG_SOURCE
+--light_lowres_denoise $LIGHT_LOWRES_DENOISE --light_bg_source $LIGHT_BG_SOURCE
