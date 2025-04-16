4eef21036e569fb53074f9ebe5ac6f49a2f4a790
Long-Giang Vu <giangvl.cs@gmail.com>
2025-04-15 23:46:46 -0700

    feat: update setup

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 apt-get update
 
 apt-get install apt-transport-https ca-certificates gnupg curl -y
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg -y
 
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 
