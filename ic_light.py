d7ee7047384e2a8e8635efcea8c05016099a058f
Long-Giang Vu <giangvl.cs@gmail.com>
2025-04-22 22:57:20 -0700

    fix

--- ic_light.py
+++ ic_light.py
@@ -426,10 +426,10 @@ if __name__ == "__main__":
         # Relight
         for camera_id in tqdm(camera_ids, desc=f"Processing Cameras for Timestep {timestep}", leave=False):
             # Get image
-            image_path = os.path.join(args.input_dir, 'images_lr', camera_id, f"{timestep}_image.png")
+            image_path = os.path.join(args.input_dir, 'images_lr', camera_id, f"{timestep}_image.jpg")
             pose_image = Image.open(image_path)
             pose_image = np.array(pose_image)
-            mask_path = os.path.join(args.input_dir, 'mask_lr', camera_id, f"{timestep}_mask.png")
+            mask_path = os.path.join(args.input_dir, 'fmask_lr', camera_id, f"{timestep}_mask.png")
             mask_image = np.array(Image.open(mask_path))
             img_masked = apply_mask(pose_image, mask_image)
             img_masked_pil = Image.fromarray(img_masked)
